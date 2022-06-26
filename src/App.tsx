3ccced2dce7060a8b04a3fdeb351d41729db4d4b
Sm0ke070 <smokedeveloper070@gmail.com>
2022-06-26 17:29:10 +0300

    created Header Navbar Profile Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar/>
+            <Profile/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer =() => {
+    return(
+        <footer>by Sm0ke</footer>
+    )
+}
+
+export default Footer;
